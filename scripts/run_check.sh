d966555ad221ffe86ed53868ade39044b63e1ca6
yoshiki iguchi <yoshiki@su.la>
2025-10-04 22:22:18 +0900

    Fix github actions #3

--- scripts/run_check.sh
+++ scripts/run_check.sh
@@ -2,6 +2,12 @@
 
 set -euxo pipefail
 
-cargo +nightly fmt --check --all -- --config error_on_line_overflow=true,error_on_unformatted=true
-cargo test
+CARGO_ARG=""
+if [ sw_vers > /dev/null 2>&1 ]; then
+  CARGO_ARG=""
+else
+  CARGO_ARG="--config target.x86_64-unknown-linux-gnu.linker=\"\""
+fi
 
+cargo +nightly fmt --check --all -- --config error_on_line_overflow=true,error_on_unformatted=true
+cargo $CARGO_ARG test
